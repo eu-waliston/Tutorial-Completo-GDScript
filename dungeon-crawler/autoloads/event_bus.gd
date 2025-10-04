8f3da1fd43900413bdcf5917337cdff306643388
waliston <wesantosdev@gmail.com>
2025-10-04 10:39:16 -0300

    update all

--- dungeon-crawler/autoloads/event_bus.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-#Sistema de eventos globais
-signal player_health_changed(old_health, new_health)
--- dungeon-crawler/main.gd
+++
@@ -1,6 +0,0 @@
-# main.gd (Cena Principal)
-extends Node2D
-
-func _ready():
-    print("🎮 Dungeon Crawler iniciado!")
-    GameManager.game_started.emit()
